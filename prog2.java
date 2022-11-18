393d74e9ba3316cf89f95e7d757bbc7572f069a5
Raktim Bar <91616950+raktimbar100@users.noreply.github.com>
2022-11-18 09:26:13 +0530

    Create prog2.java

---
+++ prog2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class ConverInPaise{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter price in rupees");
+double p=sc.nextDouble();
+int paise=(int)(p*100);
+System.out.println("price in paise is="+paise);
+}
+}
